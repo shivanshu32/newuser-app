7fe579e78c0cd9c6c20c7d1bcc8bc5ea4801b86b
Shubham Ken <shivanshu32@gmail.com>
2025-10-05 01:36:44 +0530

    savve

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = ({ config }) => ({
   jsEngine: "hermes",
   name: "Jyotish Call",
   slug: "jyotishcall-user-app",
-  version: "5.3.4",
+  version: "5.3.5",
   // Remove global orientation restriction for Android 16+ large screen support
   // orientation: "portrait", // Commented out for large screen compatibility
   icon: "./assets/icon-square.png",
@@ -35,7 +35,7 @@ module.exports = ({ config }) => ({
       backgroundColor: "#ffffff"
     },
     package: "com.jyotishtalk",
-    versionCode: 59,
+    versionCode: 60,
     minSdkVersion: 24,
     compileSdkVersion: 36,
     targetSdkVersion: 36,
